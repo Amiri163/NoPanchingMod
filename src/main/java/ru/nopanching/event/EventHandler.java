1900751310d6132b0f1ec3e657c531a08acefe70
Amiri <iuak228@gmail.com>
2023-11-11 08:17:35 +0300

    final

--- src/main/java/ru/nopanching/event/EventHandler.java
+++ src/main/java/ru/nopanching/event/EventHandler.java
@@ -6,39 +6,58 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 @Mod.EventBusSubscriber
 public class EventHandler {
     public static boolean commandBreak = true;
-    public static int tick = 0;
     private static BlockPos lastBrokenBlockPos = null;
 
     @SubscribeEvent
-    public static void onBlockHarvest(PlayerEvent.BreakSpeed event) {
+    public static void onBlockHarvest(BlockEvent.BreakEvent event) {
         Block block = event.getState().getBlock();
         String blockName = block.getRegistryName().toString();
         ItemStack heldItemStack = event.getPlayer().getMainHandItem();
 
-            if (blockName.equals("minecraft:oak_log") && !isAxe(heldItemStack) && commandBreak) {
-                BlockPos blockPos = event.getPos();
-                IWorld world = event.getEntity().getCommandSenderWorld();
+        if (isBreak(blockName) && !isAxe(heldItemStack) && commandBreak) {
+            BlockPos blockPos = event.getPos();
             if (lastBrokenBlockPos == null || !lastBrokenBlockPos.equals(blockPos)) {
-                tick = 0;
                 lastBrokenBlockPos = blockPos;
             }
-            tick++;
-            event.setNewSpeed(0.7F);
-            if (tick == 170) {
-                world.destroyBlock(blockPos, false);
-                tick = 0;
-                lastBrokenBlockPos = null;
-            }
+            event.getPlayer().getCommandSenderWorld().destroyBlock(blockPos, false);
+
+            lastBrokenBlockPos = null;
         } else {
-            tick = 0;
             lastBrokenBlockPos = null;
         }
+    }
+
+    @SubscribeEvent
+    public static void timeBreakBlock(PlayerEvent.BreakSpeed event) {
+        Block block = event.getState().getBlock();
+        String blockName = block.getRegistryName().toString();
+        ItemStack heldItemStack = event.getPlayer().getMainHandItem();
+        if (isBreak(blockName) && !isAxe(heldItemStack) && commandBreak) {
+            event.setNewSpeed(0.7F);
+        }
+
+    }
+
+    private static boolean isBreak(String blockName) {
+        String[] keywords = {"log", "wood", "fence", "planks", "stairs", "oak", "table", "plate",
+                "door", "sign", "boat", "chest", "slab", "rail", "framed", "beam", "acacia", "crimson", "spruce", "warped", "jungle", "birch"};
+
+        for (String keyword : keywords) {
+            if (blockName.contains(keyword)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static boolean isAxe(ItemStack stack) {
